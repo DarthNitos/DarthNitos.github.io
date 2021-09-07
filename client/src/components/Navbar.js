8753d537ba1ff90bd527b840dfac877e447e88f0
unknown <Nitos363@gmail.com>
2021-09-06 17:49:47 -0700

    Overlay for the dropdown menu

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,6 +6,8 @@ import "../css/main.css";
 export const Navbar = () => {
   const [isShown, setIsShown] = useState(false);
   const menuBtnRef = useRef(null);
+  const menuNav = useRef(null);
+  const menuBranding = useRef(null);
   const history = useHistory();
   const auth = useContext(AuthContext);
 
@@ -17,19 +19,28 @@ export const Navbar = () => {
   };
 
   useEffect(() => {
-    if (isShown) return menuBtnRef.current.classList.add("close");
+    if (isShown) {
+      menuBtnRef.current.classList.add("close");
+      menuNav.current.classList.add("show");
+      menuBranding.current.classList.add("show");
 
-    return menuBtnRef.current.classList.remove("close");
+      return;
+    }
+
+    menuBtnRef.current.classList.remove("close");
+    menuNav.current.classList.remove("show");
+    menuBranding.current.classList.remove("show");
   }, [isShown]);
 
   const setDisplay = {
     display: isShown ? "block" : "none",
+    visibility: isShown ? "visible" : "hidden",
   };
 
   return (
     <header>
       <nav className="navbar purple lighten-4">
-        <div className="navbar nav-wrapper">
+        <div className="nav-wrapper">
           <NavLink to="/">
             <span className="brand-logo">Linkio</span>
           </NavLink>
@@ -60,18 +71,22 @@ export const Navbar = () => {
       </div>
 
       <nav className="menu" style={setDisplay}>
-        <div className="menu-branding">
-          <div className="logo">Linkio</div>
+        <div className="menu-branding" ref={menuBranding}>
+          <NavLink to="/" className="logo nav-link" />
         </div>
-        <ul className="menu-nav">
+        <ul className="menu-nav" ref={menuNav}>
+          <li className="nav-item current">
+            <NavLink to="/create" className="nav-link">
+              Create
+            </NavLink>
+          </li>
           <li className="nav-item">
-            <NavLink to="/create">Create</NavLink>
+            <NavLink to="/links" className="nav-link">
+              My Links
+            </NavLink>
           </li>
-          <li>
-            <NavLink to="/links">My Links</NavLink>
-          </li>
-          <li>
-            <a href="/" onClick={logoutHandler}>
+          <li className="nav-item">
+            <a href="/" className="nav-link" onClick={logoutHandler}>
               Logout
             </a>
           </li>
